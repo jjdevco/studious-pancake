510c1d946afaaab160e075bbc5f384ec97c45bc9
Jose Jimenez <email@jimenezjose.com>
2021-04-25 13:10:26 -0500

    fix(queries): Require the api token

--- queries/instance.ts
+++ queries/instance.ts
@@ -4,7 +4,7 @@ const Token = process.env.NEXT_PUBLIC_GITHUB_AUTH_TOKEN;
 
 const graphqlWithAuth = graphql.defaults({
   headers: {
-    authorization: `bearer ${Token}`,
+    authorization: Token && `bearer ${Token}`,
   },
 });
 
